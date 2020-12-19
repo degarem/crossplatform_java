e9bd6b8e8d22fdc49e682ee4535d405c670ec78d
degarem <shamankingmaybe@gmail.com>
2020-12-20 00:44:08 +0300

    lab1

---
+++ laba1_java/src/com/company/Main.java
@@ -0,0 +1,41 @@
+
+
+/**@
+   * @author Semenets Vlad <adress>@shamankingmaybe@gmail.com</adress>
+ * @version 1.0
+ * @since 2020-12-13
+ */
+
+/**
+ * This is the main app class,and it contains app entry method(main)
+ */
+public class Main {
+    /**
+     * Entry point to the program
+     * @param args Unused
+     * @return Always return nothing
+     */
+
+    public static void main(String[] args) {
+
+                String[] Text = new String[] {
+                        "Group: 545a",
+                        "Student: Semenets Vladislav Semenets",
+                        "Variant: 11",
+                        " *********    ****               ****     ****       ****",
+                        "***     ***    ****             ****      *** *     * ***",
+                        "**              ****           ****       ***  *   *  ***",
+                        "  ********       ****         ****        ***   ***   ***",
+                        "         **       ****       ****         ***         *** ",
+                        "*        **        ****     ****          ***         ***",
+                        "**      **          ****   ****           ***         ***",
+                        " ********            ********             ***         ***",
+                        "  ******              *****               ***         ***"
+                };
+                for(String s : Text)
+                {
+                    System.out.println(s);
+                }
+            }
+        }
+
